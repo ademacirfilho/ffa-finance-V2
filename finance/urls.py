9869eb7707659a91d8a7290b019a621718cc0853
Ademacir Filho <ademacirfilho@gmail.com>
2025-09-17 16:02:30 -0300

    feat templates

--- finance/urls.py
+++ finance/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("receitas/", views.receitas, name="receitas"),
+    path("despesas/", views.despesas, name="despesas"),
+    path("contatos/", views.contatos, name="contatos"),
+    path("categorias/", views.categorias, name="categorias"),
+    path("usuario/", views.usuario, name="usuario"),
 ]
--- finance/views.py
+++ finance/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "finance/index.html")
+
+def receitas(request):
+    return render(request, "finance/receitas.html")
+
+def despesas(request):
+    return render(request, "finance/despesas.html")
+
+def contatos(request):
+    return render(request, "finance/contatos.html")
+
+def categorias(request):
+    return render(request, "finance/categorias.html")
+
+def usuario(request):
+    return render(request, "finance/usuario.html")
