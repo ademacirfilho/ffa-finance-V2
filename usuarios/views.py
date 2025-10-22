37d9030317f29637d37b02b867914f5088a0b9f2
Ademacir Filho <ademacirfilho@gmail.com>
2025-10-22 17:16:15 -0300

    feat: cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class UsuarioCreate(CreateView):
+    template_name = "usuarios/cadastro.html"
+    form_class = UsuarioForm
+    success_url = reverse_lazy("index")
