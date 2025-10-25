080b758318a48dcac93062ecc8100c88c959951f
Ademacir Filho <ademacirfilho@gmail.com>
2025-10-25 14:30:05 -0300

    feat(auth): impede cadastro de email repetido

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UsuarioForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,9 @@ class UsuarioForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("O email {} já está em uso.".format(email))
+        return email
