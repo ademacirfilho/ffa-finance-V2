36cceb8686943150537c5111299004258b1281c6
Ademacir Filho <ademacirfilho@gmail.com>
2025-10-28 09:45:59 -0300

    feat: criar models do usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
 
+from .models import Perfil
+
 class UsuarioCreate(CreateView):
     template_name = "usuarios/cadastro.html"
     form_class = UsuarioForm
     success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        url = super().form_valid(form)
+
+        Perfil.objects.create(usuario=self.object)
+
+        return url
