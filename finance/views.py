6e7bb6fdf708c27a5875466a654bcd6b96ef7871
Ademacir Filho <ademacirfilho@gmail.com>
2025-11-04 09:22:29 -0300

    feat: criar botão logout

--- finance/views.py
+++ finance/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "finance/index.html"
 
     def get_context_data(self, **kwargs):
